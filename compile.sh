97597aa2c71364801e495be0d58aa5b845d18a52
andy.chuang <andy.chuang@lctech.com.tw>
2025-01-08 06:49:49 +0000

    fix: use docker proceed formator
    fix: use docker proceed formator
    
    fix: use docker proceed formator

--- compile.sh
+++ compile.sh
@@ -97,7 +97,7 @@ else
     # remove TOC
     FORMATER="md-formater.sh"
     curl -sLJO "https://raw.githubusercontent.com/lctech-andychuang/protobuf-codegen-action/main/proto/$FORMATER"
-    sudo TAG_VERSION=$TAG_VERSION ./"$FORMATER" 
+    docker run -e TAG_VERSION=$TAG_VERSION --volume "$(pwd):/workspace" --workdir /workspace bash:5.2 bash "$FORMATER"  
 fi
 
 echo "@ Done 🎉🎉🎉"
