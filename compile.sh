e7d45eec3535562b1c24ec1a3cdf239bada6ab0c
andy.chuang <andy.chuang@lctech.com.tw>
2025-01-14 06:41:39 +0000

    fix: remove md-formater

--- compile.sh
+++ compile.sh
@@ -90,10 +90,6 @@ else
     sudo rm -rf ../dist/go/github.com/*
     # Restore original src
     cd .. && rm -rf src && mv tmp_src src
-
-    # remove TOC
-    # sudo -E TAG_VERSION=$TAG_VERSION curl -sL "https://raw.githubusercontent.com/lctech-andychuang/protobuf-codegen-action/main/proto/md-formater.sh" | bash
-
 fi
 
 echo "@ Done 🎉🎉🎉"
